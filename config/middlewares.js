a1cdf1af4aaf1faab68f15992cf546941aa6ad2e
ElenaGrasovskaya <elenagrasovskaya@gmail.com>
2022-06-09 12:02:22 +0300

    Linux sync

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,14 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['https://libraportal.vercel.app', 'http://localhost:3000', 'https://portal.libramebel.od.ua', "*"]
+    }
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',

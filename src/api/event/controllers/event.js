5f8abd369225121aee9699da00ebe60a2028f274
ElenaGrasovskaya <elenagrasovskaya@gmail.com>
2022-06-11 13:17:54 +0300

    Cors authorization

--- src/api/event/controllers/event.js
+++ src/api/event/controllers/event.js
@@ -6,5 +6,15 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
+
+/*module.exports = {
+    async me(ctx) {
+        const user = ctx.state.user
+
+        if(!user) {
+            return ctx.badRequest(null, [{ messages: [{id: "No authorization Header was found"}]},]);
+        }
+    }
+}*/
 module.exports = createCoreController('api::event.event');
 

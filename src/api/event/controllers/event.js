14c6952e3d753c793d5d33a37a04b08bafa5ed43
ElenaGrasovskaya <elenagrasovskaya@gmail.com>
2022-06-14 22:53:04 +0300

    portal URL

--- src/api/event/controllers/event.js
+++ src/api/event/controllers/event.js
@@ -5,16 +5,25 @@
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
+const { sanitizeEntity } = require('strapi-utils');
+//
 
 
-/*module.exports = {
+module.exports = createCoreController("api::event.event", ({ strapi }) => ({
     async me(ctx) {
         const user = ctx.state.user
 
         if(!user) {
-            return ctx.badRequest(null, [{ messages: [{id: "No authorization Header was found"}]},]);
+            return ctx.badRequest(null,
+                 [{ messages: [{id: "No authorization Header was found"}]},]);
         }
-    }
-}*/
-module.exports = createCoreController('api::event.event');
 
+        const data = await strapi.services.events.find({user: user.id})
+        if(!data) {
+            return ctx.notFound()
+        }
+        return sanitizeEntity(data, {model:strapi.models.events})
+    }
+}));
+
+module.exports = createCoreController('api::event.event');

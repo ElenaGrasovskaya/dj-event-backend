7ba444e679616241cb54cd521340c0bfb58827f2
ElenaGrasovskaya <elenagrasovskaya@gmail.com>
2022-06-24 23:15:17 +0300

    Flow

--- config/middlewares.js
+++ config/middlewares.js
@@ -9,7 +9,7 @@ module.exports = [
       origin: ['https://libraportal-backend.herokuapp.com',
        'http://localhost:1337','http://localhost:3000', 'http://localhost:3000/account/login',
        'https://portal.libramebel.od.ua/account/login', 'https://portal.libramebel.od.ua/account/login',
-        'https://portal.libramebel.od.ua', 'https://portal.libramebel.od.ua/api/login',
+        'https://portal.libramebel.od.ua', 'https://portal.libramebel.od.ua/api/login','https://portal.libramebel.od.ua/',
          'https://libraportal.vercel.app', 'https://portal.admin.libramebel.od.ua'],
       credentials: true,
     }
